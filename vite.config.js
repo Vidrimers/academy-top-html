f21dc07e302fc902198b1ef7bf585e625e7c96cc
Vidrimers <vidrimers2@gmail.com>
2025-09-14 18:49:50 +0300

    переписал readme, добавил инфу в .gitignore

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { exec } from 'child_process'
 
 export default defineConfig({
+  // base: './',
   plugins: [
     {
       name: 'custom-browser-opener',
@@ -22,13 +23,27 @@ export default defineConfig({
     port: 666,
     open: false,
   },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name][extname]',
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: 'src/assets/[name].js',
+  //       chunkFileNames: 'src/assets/[name].js',
+  //       assetFileNames: 'src/assets/[name][extname]',
+  //     },
+  //   },
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: 'assets/[name].js',
+  //       chunkFileNames: 'assets/[name].js',
+  //       assetFileNames: (assetInfo) => {
+  //         if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+  //           return 'assets/css/[name][extname]'
+  //         }
+  //         return 'src/assets/[name][extname]'
+  //       },
+  //     },
+  //   },
+  // },
 })
